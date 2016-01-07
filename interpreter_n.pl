a738e498970b488e9a0cc2299ee99b2fe76bc69e
Nicklas Andersson <nicklas@wwn.se>
2016-01-07 13:11:03 +0100

    hmm

---
+++ interpreter_n.pl
@@ -0,0 +1,112 @@
+/***
+A skeleton for Assignment 3 on PROP HT2015 at DSV/SU.
+Peter Idestam-Almquist, 2015-12-09.
+***/
+
+:- [tokenizer].
+
+/***
+Example call of the top level predicate run/2:
+?- run('program1.txt','myparsetree1.txt').
+***/
+
+run(InputFile,OutputFile):-
+	tokenize(InputFile,Program),
+	parse(ParseTree,Program,[]),
+	%evaluate(ParseTree,[],VariablesOut),
+	output_result(OutputFile,ParseTree,VariablesOut).
+
+output_result(OutputFile,ParseTree,Variables):- 
+	open(OutputFile,write,OutputStream),
+	write(OutputStream,'PARSE TREE:'), 
+	nl(OutputStream), 
+	writeln_term(OutputStream,0,ParseTree),
+	nl(OutputStream), 
+	write(OutputStream,'EVALUATION:'), 
+	nl(OutputStream), 
+	write_list(OutputStream,Variables), 
+	close(OutputStream).
+	
+writeln_term(Stream,Tabs,int(X)):-
+	write_tabs(Stream,Tabs), 
+	writeln(Stream,int(X)).
+writeln_term(Stream,Tabs,ident(X)):-
+	write_tabs(Stream,Tabs), 
+	writeln(Stream,ident(X)).
+writeln_term(Stream,Tabs,Term):-
+	functor(Term,_Functor,0), !,
+	write_tabs(Stream,Tabs),
+	writeln(Stream,Term).
+writeln_term(Stream,Tabs1,Term):-
+	functor(Term,Functor,Arity),
+	write_tabs(Stream,Tabs1),
+	writeln(Stream,Functor),
+	Tabs2 is Tabs1 + 1,
+	writeln_args(Stream,Tabs2,Term,1,Arity).
+	
+writeln_args(Stream,Tabs,Term,N,N):-
+	arg(N,Term,Arg),
+	writeln_term(Stream,Tabs,Arg).
+writeln_args(Stream,Tabs,Term,N1,M):-
+	arg(N1,Term,Arg),
+	writeln_term(Stream,Tabs,Arg), 
+	N2 is N1 + 1,
+	writeln_args(Stream,Tabs,Term,N2,M).
+	
+write_tabs(_,0).
+write_tabs(Stream,Num1):-
+	write(Stream,'\t'),
+	Num2 is Num1 - 1,
+	write_tabs(Stream,Num2).
+
+writeln(Stream,Term):-
+	write(Stream,Term), 
+	nl(Stream).
+	
+write_list(_Stream,[]). 
+write_list(Stream,[Ident = Value|Vars]):-
+	write(Stream,Ident),
+	write(Stream,' = '),
+	format(Stream,'~1f',Value), 
+	nl(Stream), 
+	write_list(Stream,Vars).
+
+	
+parse(ParseTree, Program, []):-
+    parser(Program, []).
+
+parser --> [].
+parser(ParseTree) --> assign(ParseTree).
+
+assign(assignment(ident(X), '=', E)) --> id(X), [=], expression(E), [;].
+
+expression(expression(T)) --> term(T).
+expression(expression(T, O, E)) --> term(T), (add_op(O); sub_op(O)), expression(E).	
+
+term(term(F)) --> factor(F).
+term(term(O, T)) --> (mul_op(O); div_op(O)), term(T).
+
+factor([factor, int(N)]) --> num(N).
+factor(factor( '(' , E, ')' )) --> ['('], expression(E), [')'].
+
+id(Xs, Ys):- 
+	[Z|Zs] = Xs,
+	atomic(Z),	
+	Ys = Zs.
+
+add_op --> [x].
+sub_op --> [-].
+mul_op --> [*].
+div_op --> [/].
+
+num(N) --> [N], { number(N) }.
+
+
+	
+/***
+evaluate(+ParseTree,+VariablesIn,-VariablesOut):-
+	TODO: Implement an evaluate predicate that evaluates a parse-tree and 
+	returns the state of the program after evaluation as a list of variables and 
+	their values.
+***/
+
--- test3.pl
+++ test3.pl
@@ -1,10 +1,11 @@
-parse(-ParseTree, +Program, []):-
-    parser(+Program, []).
+%parse(Tree ,Program, []):- parser(Program, []).
 
-parser --> [].
-parser --> assign.
+parser([]) --> [].
+parser(Tree) --> assign(Tree).
 
-assign(assignment(ident(X), '=', E)) --> id(X), [=], expression(E), [;].
+assign(assignment(ident(X), '=', Exp)) --> id(X), [=], expression(Exp), [;].
+
+id(X) --> [X], { atom(X) }.
 
 expression(expression(T)) --> term(T).
 expression(expression(T, O, E)) --> term(T), (add_op(O); sub_op(O)), expression(E).	
@@ -12,15 +13,44 @@ expression(expression(T, O, E)) --> term(T), (add_op(O); sub_op(O)), expression(
 term(term(F)) --> factor(F).
 term(term(O, T)) --> (mul_op(O); div_op(O)), term(T).
 
-factor(factor(I)) --> number(I).
-factor(factor('(', E, ')') --> [(], expression(E), [)].
+factor([factor, int(N)]) --> num(N).
+factor(factor( '(' , E, ')' )) --> ['('], expression(E), [')'].
 
-id(Xs, Ys):- 
+
+
+/*add_op(Op) --> [Op], { ['+'] }.
+sub_op(Op) --> [Op], { ['-'] }.
+div_op(Op) --> [Op], { ['/'] }.
+mul_op(Op) --> [Op], { ['*', '/'] }.*/
+
+sub_op(Xs, Ys):-
 	[Z|Zs] = Xs,
-	atomic(Z),	
+	sub_op_sign(Z),
 	Ys = Zs.
+	
+sub_op_sign(';').	
+	
+add_op(Xs, Ys):-
+	[Z|Zs] = Xs,
+	add_op_sign(Z),
+	Ys = Zs.
+	
+add_op_sign('+').
+	
+div_op(Xs, Ys):-
+	[Z|Zs] = Xs,
+	div_op_sign(Z),
+	Ys = Zs.
+	
+div_op_sign('/').
+	
+mul_op(Xs, Ys):-
+	[Z|Zs] = Xs,
+	mul_op_sign(Z),
+	Ys = Zs.
+	
+mul_op_sign('*').
 
-add_op --> [x].
-sub_op --> [-].
-mul_op --> [*].
-div_op --> [/].
+num(N) --> [N], { number(N) }.
+
+% [a, '=', 1, '*', 2, '+', '(', 3, '-', 4, ')', '/', 5, ';'] [O],
