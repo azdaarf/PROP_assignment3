e30efc3d7033cd2de0a4b87bf0dd91f094022f4f
Nicklas Andersson <nicklas@wwn.se>
2016-01-07 12:22:14 +0100

    ?

---
+++ test3.pl
@@ -0,0 +1,26 @@
+parse(-ParseTree, +Program, []):-
+    parsor(+Program, []).
+
+parser --> [].
+parser --> assign.
+
+assign(assignment(ident(X), '=', E)) --> id(X), [=], expression(E), [;].
+
+expression(expression(T)) --> term(T).
+expression(expression(T, O, E)) --> term(T), (add_op(O); sub_op(O)), expression(E).	
+
+term(term(F)) --> factor(F).
+term(term(O, T)) --> (mul_op(O); div_op(O)), term(T).
+
+factor(factor(I)) --> number(I).
+factor(factor('(', E, ')') --> [(], expression(E), [)].
+
+id(Xs, Ys):- 
+	[Z|Zs] = Xs,
+	atomic(Z),	
+	Ys = Zs.
+
+add_op --> [x].
+sub_op --> [-].
+mul_op --> [*].
+div_op --> [/].
